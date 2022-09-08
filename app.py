1ee369b376cdc285b531ab10fdb7228343102edc
Josh Anderson <joshand@cisco.com>
2022-09-08 16:24:36 -0500

    update flask app to read image server ip and port via environment variables

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, stream_template
 import random
 import string
+import os
+
 
 app = Flask(__name__)
+image_server_ip_address = os.environ.get('IMG_SVR_IP', "127.0.0.1")
+image_server_prt_number = os.environ.get('IMG_SVR_PRT', "8080")
 
 
 def string_num_generator(size):
@@ -12,5 +16,8 @@ def string_num_generator(size):
 
 @app.route('/')
 def hello():
-    return stream_template("home.html", rnd=string_num_generator(8))
+    return stream_template("home.html",
+                           rnd=string_num_generator(8),
+                           ip=image_server_ip_address,
+                           port=image_server_prt_number)
 
